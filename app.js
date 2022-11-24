290c4f8b6073e7e92ea3dea2aca1c0cc2fff8937
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2022-11-24 08:26:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 function Salvar() {
     let nome = document.getElementById("nome").value
-    let Sobrenome= document.getElementById("sobrenome").value
+    let sobrenome= document.getElementById("sobrenome").value
     let emal = document.getElementById("email").value
-    let linha = "<tr><td>" + nome + "</td><td>" + sobrenome + "</td><tr>"
+    let senha = document.getElementById("senha").value
+    let confirmesenha = document.getElementById("confirmesenha").value
+    let linha = "<tr><td>" + nome + "</td><td>" + sobrenome + "</td><tr>" + emal + "</tr><td>"
+    "<tr><td>" + senha + "</td><td>" + confirmesenha + "</td><tr>" + data + "</tr><td>"
     documen.getElementById("cliente").innerHTML += linha
 
 }   
