a428107ef4f43cf3e33f1b6eb7d897cc71d3e718
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2022-11-24 20:21:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,18 @@ function Salvar() {
     let email = document.getElementById("email").value
     let senha = document.getElementById("senha").value
     let confirmesenha = document.getElementById("confirmesenha").value
-    let data = document.getElementById("data").value
+    let date = document.getElementById("date").value
     let cidade = document.getElementById("cidade").value
-    let Est = document.getElementById("tEst").value
+    let Est = document.getElementById("Est").value
+    let endereco = document.getElementById("endereco").value
+    let tNum = document.getElementById("tNum").value
    
-    let linha = "<tr><td>" + nome + "</td><td>" + sobrenome + "</td><tr>" + email + "</tr><td>"
-    "<tr><td>" + senha + "</td><td>" + confirmesenha + "</td><tr>" + data + "</tr><td>"
-    "<tr><td>" + cidade + "</td><td>" + Est + "</td><tr>"
+
+   console.loge(nome, sobrenome, email, senha, confirmesenha, date, cidade, Est, endereco, tNum)
+
+    let linha = "<tr><td>" + nome + "</tr><td>" + sobrenome +"</td><tr>"  + email + "</td><td>"
+   "<tr><td>" + senha + "</td><td>" + confirmesenha + "</td><td>" + data + "</td><td>"
+    "<tr><td>" + cidade + "</td><td>" + Est + "</td><tr>" + endereco + "</td><tr>" + tNum + "</td><td>"
     documen.getElementById("cliente").innerHTML += linha
 
 }   
