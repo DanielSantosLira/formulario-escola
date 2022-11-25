fa2aefdb7a9b332f6486515fd2c6a14c115c6ce0
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2022-11-25 10:42:03 -0300

    complementando o código

--- app.js
+++ app.js
@@ -14,7 +14,16 @@ function Salvar() {
 
     let linha = "<tr><td>" + nome + "</td><td>" + sobrenome +"</td><td>"  + email + "</td><td>" + senha + "</td><td>" + confirmesenha + "</td><td>" + data + "</td><td>"  + endereco + "</td><td>" + tNum + "</td><td>"    + cidade +"</td><td>" + Est + "</td></tr>"
     document.getElementById("cliente").innerHTML += linha 
-    document.getElementById("nome", "sobrenome", "email", "senha", "confirmesenha", "date", "cidade", "Est", "endereco", "tNum").value = ""
+    document.getElementById("nome").value = ""
+    document.getElementById("sobrenome").value = ""
+    document.getElementById("email").value = ""
+    document.getElementById("senha").value = ""
+    document.getElementById("confirmesenha").value = ""
+    document.getElementById("date").value = ""
+    document.getElementById("cidade").value = ""
+    document.getElementById("Est").value = ""
+    document.getElementById("endereco").value = ""
+    document.getElementById("tNum").value = ""
 
 }   
 
